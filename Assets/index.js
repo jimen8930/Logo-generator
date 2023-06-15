0912c8dcf9acf691151765365a4d76cecfb7c340
Elizabeth Jimenez <2good2btru@ElizabethsAir3.lan1>
2023-06-15 12:51:11 -0400

    Added questions for the prompt.

--- Assets/index.js
+++ Assets/index.js
@@ -6,9 +6,26 @@ const generateLogo = require('../generateLogo.js');
 // Array of questions for user input
 const questions = [
     {
-      type: '',
-      name: '',
-      message: '',
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three caharacters for your logo.',
     },
+    {
+        type: 'list',
+        name: 'textColor',
+        message: 'Pick a text color you will like for your logo.',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Pick a shape for your logo.',
+      },
+      {
+        type: 'list',
+        name: 'shapeColor',
+        message: 'Pick a color for your shape.',
+      },
 ];
+
+// This is the function to write the svg file for the logo.
 
