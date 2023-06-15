7c06e3e815e45e19b246efcc4aa935a033da60ad
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-06-14 22:56:17 -0400

    Added a variables for the packages and fs.

---
+++ Assets/index.js
@@ -0,0 +1,13 @@
+// This includes packages needed for this application which are the file system, inquire package, and the pathway for the generate logo. 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateLogo = require('../generateLogo.js');
+
+// Array of questions for user input
+const questions = [
+    {
+      type: '',
+      name: '',
+      message: '',
+    },
+];
