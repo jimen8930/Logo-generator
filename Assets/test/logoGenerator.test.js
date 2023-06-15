c1b9be7bf263f2275209a09a3b9e5fd1a4bb676a
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-06-14 22:11:48 -0400

    Created a test file and install jest.

---
+++ Assets/test/logoGenerator.test.js
@@ -0,0 +1,4 @@
+// Constructor Arithmetic is imported.
+const generateLogo = require('../generateLogo.js');
+// A testing suite for the logo is created.
+describe("logo")
---
+++ Assets/utils/generateLogo.js
@@ -0,0 +1,7 @@
+function generateMarkdown(companyName) {
+    // Logic to generate a logo based on the company name
+    // This is a simplified example
+    return `Logo for ${companyName}`;
+  }
+  
+  module.exports = generateMarkdown;
