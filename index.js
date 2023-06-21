a0e2cc8928c55da71c15c6dd87ed26f42f4790bb
Wayne Johnson <waynejohnson@Elizabeths-MacBook-Pro.local>
2023-06-21 12:33:26 -0400

    Remove the generate logo file and the variable in the index js file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // This includes packages needed for this application which are the file system, inquire package, and the pathway for the generate logo. 
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateLogo = require('../generateLogo.js');
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
 // Array of questions for user input
--- utils/generateLogo.js
+++
@@ -1,7 +0,0 @@
-function generateLogo(companySVG) {
-    // Logic to generate a logo based on the company name
-    // This is a simplified example
-    return `Logo for ${companySVG}`;
-  }
-  
-  module.exports = generateLogo;
