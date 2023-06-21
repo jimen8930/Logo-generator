ea670ed5c85f1d70595540bcee761db98c8a41b6
Wayne Johnson <waynejohnson@Elizabeths-MacBook-Pro.local>
2023-06-20 22:15:09 -0400

    Started to add code for unit testing.

--- test/logoGenerator.test.js
+++
@@ -1,4 +0,0 @@
-// Constructor Arithmetic is imported.
-const generateLogo = require('../generateLogo.js');
-// A testing suite for the logo is created.
-describe("logo")
---
+++ test/shapes.test.js
@@ -0,0 +1,7 @@
+// Constructor Arithmetic is imported.
+const { Triangle, Square, Circle } = require("./shapes.js");
+
+// A testing suite for the logo is created.
+describe('Shape Classes', () => {
+    
+})
