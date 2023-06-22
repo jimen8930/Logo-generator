30684a85307f963f2739f0eb896bef7b1100656c
Wayne Johnson <waynejohnson@Elizabeths-MacBook-Pro.local>
2023-06-21 21:00:18 -0400

    Remove individual test files for the shapes and combine to make one test file.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,13 +1,28 @@
-// Constructor Arithmetic is imported.
-const Shape =require("../lib/shapes");
-// A testing suite for the logo is created.
-describe('Shape', () => {
-    describe ('setColor', () => {
-        it('should set the color property', () => {
-            const shape = new Shape();
-            shape.setColor('red');
-            expect(shape.color).toBe('red');
+const { Triangle, Square, Circle } = require("../lib/shapes");
 
-        });
-    });
+describe("Triangle", () => {
+  test("renders a triangle SVG with the specified color", () => {
+    const triangle = new Triangle();
+    triangle.setColor("#FF0000");
+    const renderedSvg = triangle.render().replace(/\s+/g, " ").trim();
+    expect(renderedSvg).toContain('<svg');
+  });
+});
+
+describe("Square", () => {
+  test("renders a square SVG element with optional text", () => {
+    const square = new Square();
+    square.setColor("#00FF00");
+    const renderedSvg = square.render().replace(/\s+/g, " ").trim();
+    expect(renderedSvg).toContain('<svg');
+  });
+});
+
+describe("Circle", () => {
+  test("renders a circle SVG element with optional text", () => {
+    const circle = new Circle();
+    circle.setColor("#0000FF");
+    const renderedSvg = circle.render().replace(/\s+/g, " ").trim();
+    expect(renderedSvg).toContain('<svg');
+  });
 });
